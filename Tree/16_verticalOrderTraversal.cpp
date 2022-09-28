db60178225b52a9a1ff5a5d9c22bf738f0cbc4c5
Araj <devops.araj@gmail.com>
2022-09-28 12:00:45 +0530

    Commit on dated 28Sep'22

--- Tree/16_verticalOrderTraversal.cpp
+++ Tree/16_verticalOrderTraversal.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 struct node
@@ -88,3 +87,43 @@ int main()
     }
     return 0;
 }
+
+// Second code
+/*
+
+    vector<int> verticalOrder(Node *root)
+    {
+        map<int, map<int, vector<int>>> nodes;
+        queue<pair<Node*, pair<int, int>>> q;
+        
+        vector<int> ans;
+        
+        if(root == NULL)
+            return ans;
+            
+        q.push({root, {0, 0}});
+        while(!q.empty()){
+            pair<Node*, pair<int, int>> temp = q.front();
+            q.pop();
+            Node* frontNode = temp.first;
+            int hd = temp.second.first;
+            int lvl = temp.second.second;
+            
+            nodes[hd][lvl].push_back(frontNode -> data);
+            if(frontNode-> left)
+                q.push({frontNode-> left,{hd - 1, lvl + 1}});
+            if(frontNode-> right)
+                q.push({frontNode-> right,{hd + 1, lvl + 1}});
+        }
+        
+        for(auto i : nodes){
+            for(auto j : i.second){
+                for(auto k : j.second){
+                    ans.push_back(k);
+                }
+            }
+        }
+        return ans;
+    }
+
+*/
